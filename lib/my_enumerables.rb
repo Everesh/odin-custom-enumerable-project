54e508abbcfb398a3d4c11627dc4ef2799a78d08
Everesh <everesh@proton.me>
2024-05-17 04:28:37 +0200

    Implement my_any?

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -20,6 +20,13 @@ module Enumerable
     end
     true
   end
+
+  def my_any?
+    self.my_each do |item|
+      return true if yield(item)
+    end
+    false
+  end
 end
 
 # You will first have to define my_each
